9fa3b703b3a382d07be03dfbe3c1dae6c25a0152
Rahul Sharma <rahulsrma26@gmail.com>
2020-09-30 00:01:30 +0530

    :arrow_right: Only buttons will have links to the post instead of full div

--- src/components/blogPreview/index.js
+++ src/components/blogPreview/index.js
@@ -11,25 +11,26 @@ const BlogPreview = ({ data }) => {
 
     return (
         <BlogPreviewStyle>
-            <Link to={postUrl}>
-                <div className="date">
-                    <span><Moment date={data.frontmatter.date} format="MMMM DD, YYYY" /></span>
-                </div>
-                <div className="preview">
-                    <img src={data.frontmatter.image.publicURL} />
-                </div>
-                <h3>{data.frontmatter.title}</h3>
-                <ul>
-                    {data.frontmatter.tags.map(tag => (
-                        <li key={tag} className="tag">{tag}</li>
-                    ))}
-                </ul>
-                <p className="description">{data.frontmatter.description}</p>
-                <div className="links">
+        
+            <div className="date">
+                <span><Moment date={data.frontmatter.date} format="MMMM DD, YYYY" /></span>
+            </div>
+            <div className="preview">
+                <img src={data.frontmatter.image.publicURL} alt={data.frontmatter.title + " preview"} />
+            </div>
+            <h3>{data.frontmatter.title}</h3>
+            <ul>
+                {data.frontmatter.tags.map(tag => (
+                    <li key={tag} className="tag">{tag}</li>
+                ))}
+            </ul>
+            <p className="description">{data.frontmatter.description}</p>
+            <div className="links">
+                <Link to={postUrl}>
                     <button className="accentBtn readMore">Read More →</button>
-                    <CommentBox url={postUrl} title={data.frontmatter.title} countOnly={true}/>
-                </div>
-            </Link>
+                </Link>
+                <CommentBox url={postUrl} title={data.frontmatter.title} countOnly={true}/>
+            </div>
         </BlogPreviewStyle>
     )
 }
